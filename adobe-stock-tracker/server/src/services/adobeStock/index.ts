443d50040b1402aa48dd4897468f6e5030257ebf
nahid1194-tech <nahidrana1194@gmail.com>
2026-08-11 11:23:09 +0600

    Delete adobe-stock-tracker directory

--- adobe-stock-tracker/server/src/services/adobeStock/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './adobeStockTypes';
-export * from './adobeStockApiClient';
-export * from './adobeStockApiProvider';
-export * from './adobeStockSearchUrlBuilder';
-export * from './dataProvider';
-export * from './searchLinkProvider';
-export * from './stats';
